cfa5f844cc1c5a789de9ea13020979e98dfb9980
uroozing <urooz.fatima2024@lpu.in>
2025-12-09 22:32:39 +0530

    Add files via upload
    This is my basic project which I have made during my learning journey of html css & javascript.
    I made it to learn events.

---
+++ color scheme.js
@@ -0,0 +1,23 @@
+const buttons=document.querySelectorAll('.button')
+const body = document.querySelector("body")
+console.log(buttons);
+buttons.forEach(function(button){
+    console.log(button);
+
+    button.addEventListener('click',function(e){
+        console.log(e)
+        console.log(e.target)
+
+        if(e.target.id==='grey'){
+            body.style.backgroundColor=e.target.id;
+        }else if(e.target.id==='white'){
+            body.style.backgroundColor=e.target.id;
+        }else if(e.target.id==='yellow'){
+            body.style.backgroundColor=e.target.id;
+        }else if(e.target.id==='purple'){
+            body.style.backgroundColor=e.target.id;
+        }else if(e.target.id==='pink'){
+            body.style.backgroundColor=e.target.id;
+        }
+    })
+});
